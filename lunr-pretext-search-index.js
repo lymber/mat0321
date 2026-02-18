5227000ef1735714b044f96490f2a748c1d89865
Alexandre Lymberopoulos <lymber@ime.usp.br>
2026-02-18 00:57:03 -0300

    Updated wesite.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,22 +1,40 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "syllabus",
+  "id": "info",
   "level": "1",
-  "url": "syllabus.html",
+  "url": "info.html",
   "type": "Seção",
   "number": "",
-  "title": "Ementa",
-  "body": " Ementa   Bem vindos ao curso de Cálculo Integral, oferecido para os alunos do Bacharelado em Matemática do IME-USP.     Informações da disciplina  Este é o conjunto de informações para o Cálculo Integral (MAT0321) no primeiro semestre de 2026, que consiste de 4 créditos-aula, ou seja 4 horas-aula semanais, totalizando 60 horas-aula.    Ministrante  Prof. Alexandre Lymberopoulos, Sala 151-A, lymber@ime.usp.br .    Aulas  Terças: 08:00 - 09:40 (Sala B-xx)  Quintas: 10:00 - 11:40 (Sala B-xx)    Horários de Atendimento:  A definir    Monitoria  A definir    Descrição e Ementa  Objetivos: Estudo da integral de Riemann em , integração de formas diferenciais, Teorema de Stokes.  Conteúdo Programático:  Integral de Riemann em . Integrabilidade de funções contínuas. Critérios de integrabilidade.Demonstração do teorema de mudança de variáveis e de Fubini.  Formas diferenciais em . Campos vetoriais. Relação entre formas e operadores vetoriais.  Teorema de Stokes (em linguagem de formas diferenciais). Aplicações à análise vetorial clássica.  Formas exatas e formas fechadas.  Tópicos adicionais se o tempo permitir.      Pré-requisitos  MAT0311 - Cálculo Diferencial e Integral V    Bibliografia   Calculus on Manifolds: A Modern Approach to Classical Theorems of Advanced Calculus de Michael Spivak.   Analisys on Manifolds de James R. Munkres.   Um curso de Análise, volume 2 de Elon Lages Lina.       Descrição do Curso  Neste curso vamos generalizar, na verdade unificar, os teoremas do cálculo vetorial vistos para curvas e superfícies em e , a saber os teoremas de Green, Gauss e Stokes. Para isso começamos retomando os principais resultados sobre funções diferenciáveis de em : os teoremas da função inversa, da função implícita, do posto e as formas locais das imersões e submersões.  Passamos em seguida para a teoria de integração para funções examinando os teoremas de Fubini e mudança de variáveis nesse contexto. Tudo isso é bastante análogo ao que já foi visto em cursos anteriores.  A novidade começa com a teoria de formas diferenciais, para as quais começamos estudando aplicações multilineares do ponto de vista algébrico para só então \"temperar\" isso com derivadas e campos vetoriais. Em seguida estudamos as subvariedades do e condições para orientabilidade das mesmas.  Com isso podemos definir a integral de uma forma diferencial sobre uma variedade, que é uma maneira de estudar objetos geométricos e analíticos de maneira global. Finalmente enunciamos o teorema de Stokes e vai ser muito legal que ele é nada mais do que uma generalização do Teorema Fundamental do Cálculo, visto no primeiro semestre da graduação.  Se o tempo permitir, vamos estudar a relação entre condições sobre formas diferenciais e a topologia do espaço onde elas estão definidas, através dos grupos de cohomologia de de Rham.      Avaliação e Notas  A avaliação do curso consistirá de três provas e a entrega periódica de exercícios.    Provas:   P 1 : data   P 2 : data   P 3 : data    Exercícios:  Veja em Entregas .      Notas na disciplina:  Veja aqui .     "
+  "title": "Informações da disciplina",
+  "body": " Informações da disciplina  Este é o conjunto de informações para o Cálculo Integral (MAT0321) no primeiro semestre de 2026, que consiste de 4 créditos-aula, ou seja 4 horas-aula semanais, totalizando 60 horas-aula.    Ministrante  Prof. Alexandre Lymberopoulos, Sala 151-A, lymber@ime.usp.br .    Aulas  Terças: 08:00 - 09:40 (Sala B-xx)  Quintas: 10:00 - 11:40 (Sala B-xx)    Horários de Atendimento  A definir    Monitoria  A definir    Descrição e Ementa  Objetivos: Estudo da integral de Riemann em , integração de formas diferenciais, Teorema de Stokes.  Conteúdo Programático:  Integral de Riemann em . Integrabilidade de funções contínuas. Critérios de integrabilidade.Demonstração do teorema de mudança de variáveis e de Fubini.  Formas diferenciais em . Campos vetoriais. Relação entre formas e operadores vetoriais.  Teorema de Stokes (em linguagem de formas diferenciais). Aplicações à análise vetorial clássica.  Formas exatas e formas fechadas.  Tópicos adicionais se o tempo permitir.      Pré-requisitos  MAT0311 - Cálculo Diferencial e Integral V    Bibliografia   Calculus on Manifolds: A Modern Approach to Classical Theorems of Advanced Calculus de Michael Spivak.   Analisys on Manifolds de James R. Munkres.   Um curso de Análise, volume 2 de Elon Lages Lina.     "
 },
 {
-  "id": "sec-course-info-2",
+  "id": "info-2",
   "level": "2",
-  "url": "syllabus.html#sec-course-info-2",
+  "url": "info.html#info-2",
   "type": "Parágrafo (with a defined term)",
   "number": "",
   "title": "",
   "body": "Cálculo Integral (MAT0321) "
+},
+{
+  "id": "descricao",
+  "level": "1",
+  "url": "descricao.html",
+  "type": "Seção",
+  "number": "",
+  "title": "Descrição do Curso",
+  "body": "  Descrição do Curso  Neste curso vamos generalizar, na verdade unificar, os teoremas do cálculo vetorial vistos para curvas e superfícies em e , a saber os teoremas de Green, Gauss e Stokes. Para isso começamos retomando os principais resultados sobre funções diferenciáveis de em : os teoremas da função inversa, da função implícita, do posto e as formas locais das imersões e submersões.  Passamos em seguida para a teoria de integração para funções examinando os teoremas de Fubini e mudança de variáveis nesse contexto. Tudo isso é bastante análogo ao que já foi visto em cursos anteriores.  A novidade começa com a teoria de formas diferenciais, para as quais começamos estudando aplicações multilineares do ponto de vista algébrico para só então \"temperar\" isso com derivadas e campos vetoriais. Em seguida estudamos as subvariedades do e condições para orientabilidade das mesmas.  Com isso podemos definir a integral de uma forma diferencial sobre uma variedade, que é uma maneira de estudar objetos geométricos e analíticos de maneira global. Finalmente enunciamos o teorema de Stokes e vai ser muito legal que ele é uma generalização do Teorema Fundamental do Cálculo, visto no primeiro semestre da graduação.  Se o tempo permitir, vamos estudar a relação entre condições sobre formas diferenciais e a topologia do espaço onde elas estão definidas, através dos grupos de cohomologia de de Rham.  "
+},
+{
+  "id": "aval",
+  "level": "1",
+  "url": "aval.html",
+  "type": "Seção",
+  "number": "",
+  "title": "Avaliação e Notas",
+  "body": " Avaliação e Notas  A avaliação do curso consistirá de duas provas e a entrega periódica de exercícios:     Provas   P 1 : data   P 2 : data    Exercícios  Veja em Entregas .     O cálculo da média de primeira avaliação é dado por:  é a média aritimética das provas e é a média dos exercícios, depois de descartada a pior nota dentre esses exercícios.  Ao aluno que faltar em uma das provas será ofertada uma avaliação substitutiva, cuja nota entra no lugar daquela onde houve a ausência.     Notas  Veja aqui .     "
 },
 {
   "id": "notes-week-01",
@@ -24,8 +42,8 @@ var ptx_lunr_docs = [
   "url": "notes-week-01.html",
   "type": "Seção",
   "number": "",
-  "title": "Week 1",
-  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+  "title": "Semana 01",
+  "body": " Semana 01   Linhas gerais do que foi coberto na primeira semana de aulas.     24\/02 Terça      26\/02 Quinta     "
 },
 {
   "id": "notes-week-02",
@@ -33,8 +51,8 @@ var ptx_lunr_docs = [
   "url": "notes-week-02.html",
   "type": "Seção",
   "number": "",
-  "title": "Week 2",
-  "body": " Week 2   Monday      Wednesday      Friday     "
+  "title": "Semana 02",
+  "body": " Semana 02   Linhas gerais do que foi coberto na segunda semana de aulas.     02\/03 Terça      04\/03 Quinta     "
 },
 {
   "id": "activity-01-intro-activity",
@@ -42,8 +60,8 @@ var ptx_lunr_docs = [
   "url": "activity-01-intro-activity.html",
   "type": "Lista de Exercícios",
   "number": "",
-  "title": "Introduction Activity",
-  "body": " Introduction Activity    This is the introduction to the activity.      This is the first exercise.    "
+  "title": "Lista 01",
+  "body": " Lista 01    Primeira Lista de Exercícios.      This is the first exercise.    Uma boa dica      This is the second exercise.    Uma boa dica    "
 },
 {
   "id": "activity-01-intro-activity-3",
@@ -52,7 +70,16 @@ var ptx_lunr_docs = [
   "type": "Exercício da Lista",
   "number": "1",
   "title": "",
-  "body": "  This is the first exercise.   "
+  "body": "  This is the first exercise.    Uma boa dica   "
+},
+{
+  "id": "activity-01-intro-activity-4",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-4",
+  "type": "Exercício da Lista",
+  "number": "2",
+  "title": "",
+  "body": "  This is the second exercise.    Uma boa dica   "
 },
 {
   "id": "handouts-3",
@@ -69,8 +96,8 @@ var ptx_lunr_docs = [
   "url": "homework-2.html",
   "type": "Lista de Exercícios",
   "number": "",
-  "title": "Homework 01",
-  "body": " Homework 01    Instructions: Complete all the exercises below and submit your work by the due date.      This is the first homework exercise.     "
+  "title": "Atividade 01",
+  "body": " Atividade 01    Instruções: Resolva os exercícios abaixo e entregue-os dentro do prazo.      This is the first homework exercise.     "
 },
 {
   "id": "homework-2-3-1",
