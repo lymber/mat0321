5a14414cd77d2665914a2f98d35acd01c6dd5ba6
Alexandre Lymberopoulos <lymber@ime.usp.br>
2026-02-18 14:59:41 -0300

    Updated website.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -37,72 +37,216 @@ var ptx_lunr_docs = [
   "body": " Avaliação e Notas  A avaliação do curso consistirá de duas provas e a entrega periódica de exercícios:     Provas   P 1 : data   P 2 : data    Exercícios  Veja em Entregas .     O cálculo da média de primeira avaliação é dado por:  é a média aritimética das provas e é a média dos exercícios, depois de descartada a pior nota dentre esses exercícios.  Ao aluno que faltar em uma das provas será ofertada uma avaliação substitutiva, cuja nota entra no lugar daquela onde houve a ausência.  Caso , o aluno tem direito a uma prova de recuperação, , que será sua nota de segunda avaliação.     Notas  Veja aqui .     "
 },
 {
-  "id": "notes-week-01",
+  "id": "cronograma-semana-01",
   "level": "1",
-  "url": "notes-week-01.html",
+  "url": "cronograma-semana-01.html",
   "type": "Seção",
   "number": "",
   "title": "Semana 01",
   "body": " Semana 01   Linhas gerais do que foi coberto na primeira semana de aulas.     24\/02 Terça      26\/02 Quinta     "
 },
 {
-  "id": "notes-week-02",
+  "id": "cronograma-semana-02",
   "level": "1",
-  "url": "notes-week-02.html",
+  "url": "cronograma-semana-02.html",
   "type": "Seção",
   "number": "",
   "title": "Semana 02",
   "body": " Semana 02   Linhas gerais do que foi coberto na segunda semana de aulas.     02\/03 Terça      04\/03 Quinta     "
 },
 {
-  "id": "activity-01-intro-activity",
+  "id": "lista01",
   "level": "1",
-  "url": "activity-01-intro-activity.html",
+  "url": "lista01.html",
   "type": "Lista de Exercícios",
   "number": "",
   "title": "Lista 01",
-  "body": " Lista 01    Primeira Lista de Exercícios.      This is the first exercise.    Uma boa dica      This is the second exercise.    Uma boa dica    "
+  "body": " Lista 01    Primeira Lista de Exercícios.       Justifique com detalhes a validade da propriedade (4) utilizada na demonstração feita em sala do Teorema da Função Inversa.      Seja dada por .  Mostre que é injetora no conjunto .  Descreva o conjunto .  Denotando por a inversa de , determine, se possível, .        Repita o exercício acima para dada por , sendo o conjunto .      Considere a função dada por   Mostre que a equação define uma função de classe em todo o plano.  Determine os pontos críticos de .  Classifique esses pontos críticos quanto a máximos locais, mínimos locais ou selas.  Escreva o polinômio de Taylor de ordem 1 e seu resto para em torno de .        Seja uma função contínua tal que . Mostre que existe função , de classe , tal que .      Identificando os espaço das matrizes quadradas de ordem , , com definimos a função por .  Mostre que é de classe .  Determine para cada .  Mostre que, numa vizinhança de , cada matriz possui uma única raiz quadrada, isto é, para cada nessa vizinhança existe uma única matriz tal que .        Seja .  Mostre que é, localmente, gráfico de uma função de classe .  Determine a dimensão do plano tangente em cada ponto desse gráfico.  Explicite o plano tangente a esse gráfico em .        Seja .  Mostre que é, localmente, gráfico de uma função real de classe .  Determine a dimensão do plano tangente a esse gráfico em .        Seja um polinômio com coeficientes reais tal que é uma raiz real simples de .  Mostre que todo polinômio de grau ``suficientemente próximo'' de tem uma raiz real pŕoxima de que varia suavemente em termos dos coeficientes do polinômio.  Determine a diferencial dessa função que associa os coeficientes do polinômio a essa raiz simples.    Pode-se obter resultado semelhante se a raiz de não é simples? Dê exemplos.      Seja uma função de classe . Para cada considere a função dada por . Suponha que exista tal que tem um ponto fixo , isto é, e que é uma transformação linear que não tem como autovalor. Mostre que, para suficientemente próximo de a função tem um único ponto fixo próximo de que varia suavemente com .      Demonstre a seguinte versão da forma local das imersões:   Se , de classe no aberto , é uma imersão em então existe um difeomorfismo entre os abertos , contendo e , contendo , tal que para todo .         Mostre que se é de classe no aberto com então não é injetora.      Seja uma função de classe no aberto . Um vetor é valor regular de se é sobrejetora, para todo .  Mostre que o gráfico de qualquer função é a imagem inversa de um valor regular.      Mostre que a curva em dada pelo sistema não é a imagem inversa de um valor regular.      O cone é imagem inversa de valor regular?      Seja dada por . Mostre que é valor regular de e descreva .  Mostre ainda que é gráfico de uma função numa vizinhança de . Determine o plano tangente ao gráfico de nesse ponto.      Mostre que se é valor regular de então, para cada existem abertos e tais que é o gráfico de uma função de classe .      Sejam uma imersão injetora de classe e uma curva de classe tal que .  Mostre que existem e , de classe tal que .    "
 },
 {
-  "id": "activity-01-intro-activity-3",
+  "id": "lista01-3",
   "level": "2",
-  "url": "activity-01-intro-activity.html#activity-01-intro-activity-3",
+  "url": "lista01.html#lista01-3",
   "type": "Exercício da Lista",
   "number": "1",
   "title": "",
-  "body": "  This is the first exercise.    Uma boa dica   "
+  "body": "   Justifique com detalhes a validade da propriedade (4) utilizada na demonstração feita em sala do Teorema da Função Inversa.   "
 },
 {
-  "id": "activity-01-intro-activity-4",
+  "id": "lista01-4",
   "level": "2",
-  "url": "activity-01-intro-activity.html#activity-01-intro-activity-4",
+  "url": "lista01.html#lista01-4",
   "type": "Exercício da Lista",
   "number": "2",
   "title": "",
-  "body": "  This is the second exercise.    Uma boa dica   "
+  "body": "  Seja dada por .  Mostre que é injetora no conjunto .  Descreva o conjunto .  Denotando por a inversa de , determine, se possível, .     "
 },
 {
-  "id": "handouts-3",
+  "id": "lista01-5",
+  "level": "2",
+  "url": "lista01.html#lista01-5",
+  "type": "Exercício da Lista",
+  "number": "3",
+  "title": "",
+  "body": "  Repita o exercício acima para dada por , sendo o conjunto .   "
+},
+{
+  "id": "lista01-6",
+  "level": "2",
+  "url": "lista01.html#lista01-6",
+  "type": "Exercício da Lista",
+  "number": "4",
+  "title": "",
+  "body": "  Considere a função dada por   Mostre que a equação define uma função de classe em todo o plano.  Determine os pontos críticos de .  Classifique esses pontos críticos quanto a máximos locais, mínimos locais ou selas.  Escreva o polinômio de Taylor de ordem 1 e seu resto para em torno de .     "
+},
+{
+  "id": "lista01-7",
+  "level": "2",
+  "url": "lista01.html#lista01-7",
+  "type": "Exercício da Lista",
+  "number": "5",
+  "title": "",
+  "body": "  Seja uma função contínua tal que . Mostre que existe função , de classe , tal que .   "
+},
+{
+  "id": "lista01-8",
+  "level": "2",
+  "url": "lista01.html#lista01-8",
+  "type": "Exercício da Lista",
+  "number": "6",
+  "title": "",
+  "body": "  Identificando os espaço das matrizes quadradas de ordem , , com definimos a função por .  Mostre que é de classe .  Determine para cada .  Mostre que, numa vizinhança de , cada matriz possui uma única raiz quadrada, isto é, para cada nessa vizinhança existe uma única matriz tal que .     "
+},
+{
+  "id": "lista01-9",
+  "level": "2",
+  "url": "lista01.html#lista01-9",
+  "type": "Exercício da Lista",
+  "number": "7",
+  "title": "",
+  "body": "  Seja .  Mostre que é, localmente, gráfico de uma função de classe .  Determine a dimensão do plano tangente em cada ponto desse gráfico.  Explicite o plano tangente a esse gráfico em .     "
+},
+{
+  "id": "lista01-10",
+  "level": "2",
+  "url": "lista01.html#lista01-10",
+  "type": "Exercício da Lista",
+  "number": "8",
+  "title": "",
+  "body": "  Seja .  Mostre que é, localmente, gráfico de uma função real de classe .  Determine a dimensão do plano tangente a esse gráfico em .     "
+},
+{
+  "id": "lista01-11",
+  "level": "2",
+  "url": "lista01.html#lista01-11",
+  "type": "Exercício da Lista",
+  "number": "9",
+  "title": "",
+  "body": "  Seja um polinômio com coeficientes reais tal que é uma raiz real simples de .  Mostre que todo polinômio de grau ``suficientemente próximo'' de tem uma raiz real pŕoxima de que varia suavemente em termos dos coeficientes do polinômio.  Determine a diferencial dessa função que associa os coeficientes do polinômio a essa raiz simples.    Pode-se obter resultado semelhante se a raiz de não é simples? Dê exemplos.   "
+},
+{
+  "id": "lista01-12",
+  "level": "2",
+  "url": "lista01.html#lista01-12",
+  "type": "Exercício da Lista",
+  "number": "10",
+  "title": "",
+  "body": "  Seja uma função de classe . Para cada considere a função dada por . Suponha que exista tal que tem um ponto fixo , isto é, e que é uma transformação linear que não tem como autovalor. Mostre que, para suficientemente próximo de a função tem um único ponto fixo próximo de que varia suavemente com .   "
+},
+{
+  "id": "lista01-13",
+  "level": "2",
+  "url": "lista01.html#lista01-13",
+  "type": "Exercício da Lista",
+  "number": "11",
+  "title": "",
+  "body": "  Demonstre a seguinte versão da forma local das imersões:   Se , de classe no aberto , é uma imersão em então existe um difeomorfismo entre os abertos , contendo e , contendo , tal que para todo .      "
+},
+{
+  "id": "lista01-14",
+  "level": "2",
+  "url": "lista01.html#lista01-14",
+  "type": "Exercício da Lista",
+  "number": "12",
+  "title": "",
+  "body": "  Mostre que se é de classe no aberto com então não é injetora.   "
+},
+{
+  "id": "lista01-15",
+  "level": "2",
+  "url": "lista01.html#lista01-15",
+  "type": "Exercício da Lista",
+  "number": "13",
+  "title": "",
+  "body": "  Seja uma função de classe no aberto . Um vetor é valor regular de se é sobrejetora, para todo .  Mostre que o gráfico de qualquer função é a imagem inversa de um valor regular.   "
+},
+{
+  "id": "lista01-16",
+  "level": "2",
+  "url": "lista01.html#lista01-16",
+  "type": "Exercício da Lista",
+  "number": "14",
+  "title": "",
+  "body": "  Mostre que a curva em dada pelo sistema não é a imagem inversa de um valor regular.   "
+},
+{
+  "id": "lista01-17",
+  "level": "2",
+  "url": "lista01.html#lista01-17",
+  "type": "Exercício da Lista",
+  "number": "15",
+  "title": "",
+  "body": "  O cone é imagem inversa de valor regular?   "
+},
+{
+  "id": "lista01-18",
+  "level": "2",
+  "url": "lista01.html#lista01-18",
+  "type": "Exercício da Lista",
+  "number": "16",
+  "title": "",
+  "body": "  Seja dada por . Mostre que é valor regular de e descreva .  Mostre ainda que é gráfico de uma função numa vizinhança de . Determine o plano tangente ao gráfico de nesse ponto.   "
+},
+{
+  "id": "lista01-19",
+  "level": "2",
+  "url": "lista01.html#lista01-19",
+  "type": "Exercício da Lista",
+  "number": "17",
+  "title": "",
+  "body": "  Mostre que se é valor regular de então, para cada existem abertos e tais que é o gráfico de uma função de classe .   "
+},
+{
+  "id": "lista01-20",
+  "level": "2",
+  "url": "lista01.html#lista01-20",
+  "type": "Exercício da Lista",
+  "number": "18",
+  "title": "",
+  "body": "  Sejam uma imersão injetora de classe e uma curva de classe tal que .  Mostre que existem e , de classe tal que .   "
+},
+{
+  "id": "resumos-3",
   "level": "1",
-  "url": "handouts-3.html",
+  "url": "resumos-3.html",
   "type": "Material de apoio",
   "number": "",
   "title": "Integração em <span class=\"process-math\">\\(\\R^n\\)<\/span> – Definições e Primeiros Resultados",
   "body": " Integração em  Definições e Primeiros Resultados   A first paragraph with some space for notes below it.   "
 },
 {
-  "id": "homework-2",
+  "id": "tarefas-2",
   "level": "1",
-  "url": "homework-2.html",
+  "url": "tarefas-2.html",
   "type": "Lista de Exercícios",
   "number": "",
   "title": "Atividade 01",
   "body": " Atividade 01    Instruções: Resolva os exercícios abaixo e entregue-os dentro do prazo.      This is the first homework exercise.     "
 },
 {
-  "id": "homework-2-3-1",
+  "id": "tarefas-2-3-1",
   "level": "2",
-  "url": "homework-2.html#homework-2-3-1",
+  "url": "tarefas-2.html#tarefas-2-3-1",
   "type": "Exercício da Lista",
   "number": "1",
   "title": "",
